0fcb50d209da38bd03de84c04d96a9f5e32c9166
Naod Taddese <naodtds@gmail.com>
2023-07-18 12:52:04 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,16 +16,16 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (k < 10 && j != 10)
-			{
-				_putchar(' ');
-				_putchar((k % 10) + '0');
-			}
-			else if (k >= 0)
+			if (k >= 0)
 			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
+			else if (k < 10 && j != 10)
+                        {
+				_putchar(' ');
+				_putchar((k % 10) + '0');
+                        }
 			else
 			{
 				_putchar((k % 10) + '0');
