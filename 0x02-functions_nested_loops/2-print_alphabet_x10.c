cea5610748c3174f90b5300e539dd417eca384a7
Naod Taddese <naodtds@gmail.com>
2023-07-18 10:29:07 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times.
+ *
+ * Return: Always 0.
+ *
+ */ 
+void print_alphabet-x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
