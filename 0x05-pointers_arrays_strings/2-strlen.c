e822140865805bdeef0e11bad9fbc193c8480bd5
Naod Taddese <naodtds@gmail.com>
2023-07-24 05:21:03 -0700

    function that displays the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - takes string and returns the length of the string
+ *
+ * @s: the variable that strlen receives
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
