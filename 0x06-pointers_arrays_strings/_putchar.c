f7fbf4ae136275bebcfe13447ceb18113bcbc3c2
Naod Taddese <naodtds@gmail.com>
2023-07-27 11:10:49 -0700

    update

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - a function that prints given char to stdout
+ *
+ * @c: the char to print
+ * Return: 0 onsuccess 1 on error
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
