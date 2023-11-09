512fcca3a34c182a1e1c060d820a771c24d1d93c
Naod Taddese <naodtds@gmail.com>
2023-11-09 15:41:42 +0300

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements
+ * @h: pointer to head
+ * Return: Number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int num = 0
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
