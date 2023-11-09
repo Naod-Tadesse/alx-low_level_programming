2e49e2c349a57daec40f012efecc5dbd449ae560
Naod Taddese <naodtds@gmail.com>
2023-11-09 15:02:50 +0300

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that print all the elements of a list
+ * @h: pointer to head
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		num++;
+	}
+
+	return (num);
+}
