35529a914364d88cbcddc45a3a25f27ccabf3619
Naod-Tadesse <naodtds@gmail.com>
2023-11-13 11:33:47 -0800

    task 100

---
+++ 0x18-dynamic_libraries/arthmetic.c
@@ -0,0 +1,59 @@
+/**
+ * mul - mul two nums
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * add - add two nums
+ * @a: first
+ * @b: second
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * sub - difference between two nums
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - mod of two nums
+ * @a: first
+ * @b: second
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
