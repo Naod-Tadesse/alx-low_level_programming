520df6ad0c7463be49c9b254e8e458f1ea1cefa1
Naod Taddese <naodtds@gmail.com>
2023-12-02 03:30:30 +0300

    test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int index, rt;
+	unsigned int index;
+	int rt = 0;
 	hash_node_t *exists, *create_new;
 	char *cp;
 
@@ -43,12 +44,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		create_new = malloc(sizeof(hash_node_t));
 		if (!create_new)
 		{
+			rt = 0;
 			free(cp);
 			return (0);
 		}
 		create_new->key = strdup(key);
 		if (!create_new->key)
 		{
+			rt = 0;
 			free(create_new);
 			return (0);
 		}
