7a08bb229901c10204a703bc60f9e4d023b044e3
Naod Taddese <naodtds@gmail.com>
2023-12-02 03:28:05 +0300

    make gcc warnings happy

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int index;
+	unsigned int index, rt;
 	hash_node_t *exists, *create_new;
 	char *cp;
 
@@ -38,7 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			exists = exists->next;
 		}
 	} 
-	
+	else
+	{
 		create_new = malloc(sizeof(hash_node_t));
 		if (!create_new)
 		{
@@ -54,6 +55,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		create_new->next = ht->array[index];
 		create_new->value = cp;
 		ht->array[index] = create_new;
-		return (1);
-	
+		rt = 1;
+	}
+	return (rt);
 }
