39d8268fda9e569847aee53c2b53c9062a57c93f
Naod Taddese <naodtds@gmail.com>
2024-03-15 11:27:46 +0300

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - this searches for value
+ * @array: the array to search the value in
+ * @size: size of the array
+ * @value: the value to search
+ * Return: the index of the value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	if (array != NULL)
+	{
+		while (i < (int)size)
+		{
+			printf("value checked arrya[%u] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+			i++;
+		}
+	}
+	return (-1);
+}
