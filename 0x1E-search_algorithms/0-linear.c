26106a7637b88b9435aca3b18faa17c60745d747
Naod Taddese <naodtds@gmail.com>
2024-03-15 11:51:33 +0300

    done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	for (i = 0; i < (int)size; i++)
+
+	for (i = 0; i < size; i++)
 	{
-		printf("value checked array[%u] = [%d]\n", i, array[i]);
+		printf("value checked array[%li] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
